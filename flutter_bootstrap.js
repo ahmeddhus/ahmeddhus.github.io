1ac01ca1e26258316d02dca3659fafd198aedc04
Ahmed <ahmed.hussein@zarcony.dev>
2024-11-23 13:56:51 +0200

    Update UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1681539707"
+    serviceWorkerVersion: "3539491723"
   }
 });
