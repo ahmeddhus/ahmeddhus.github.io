ec2aa4c4499b5b6eda8d10fed589db06992a4a62
Ahmed <ahmed.hussein@zarcony.dev>
2024-11-30 13:43:36 +0200

    fix share screen option

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8d44213816cc1529959b17f536447b18",
+const RESOURCES = {"flutter_bootstrap.js": "64632d884892229c55f60b6e13d6e6b6",
 "version.json": "5ffb5b67a0731739b3a1d2361cc7cef8",
 "index.html": "b277eb1485b105224ea022a7d03e3978",
 "/": "b277eb1485b105224ea022a7d03e3978",
-"main.dart.js": "00ad4d6f53766e6186104ec25072aa20",
+"main.dart.js": "ee8aa4dd9f1014782d13c32033df7dd6",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5806fae7146f865965868e5514f645b0",
 "icons/Icon-192.png": "91e21ff102cceb234132769092debdd8",
