f00c1cdb0d1838344315bbf12ca0723859f4f962
Ahmed <ahmed.hussein@zarcony.dev>
2024-11-30 13:52:42 +0200

    fix share screen option

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2613685218"
+    serviceWorkerVersion: "4194067387"
   }
 });
